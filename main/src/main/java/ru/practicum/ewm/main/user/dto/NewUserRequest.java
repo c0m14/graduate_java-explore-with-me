8938e17579e9c8745795b708cc2eae2efce05265
Nikita Komochkov <ascii2@yandex.ru>
2023-08-10 15:53:35 +0300

    fix: add email max length validation

--- main/src/main/java/ru/practicum/ewm/main/user/dto/NewUserRequest.java
+++ main/src/main/java/ru/practicum/ewm/main/user/dto/NewUserRequest.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -11,10 +12,10 @@ public class NewUserRequest {
 
     @NotNull
     @Email
-    @Length(min = 6)
+    @Length(min = 6, max = 254)
     private String email;
 
-    @NotNull
+    @NotBlank
     @Length(min = 2, max = 250)
     private String name;
 }
