b768a8d0faa937205e35791d87ed7e335c023871
Nikita Komochkov <ascii2@yandex.ru>
2023-08-21 21:13:57 +0300

    fix: unused imports

--- statistic/dto/src/main/java/ru/practicum/ewm/statistic/dto/Formats.java
+++ statistic/dto/src/main/java/ru/practicum/ewm/statistic/dto/Formats.java
@@ -1,7 +1,5 @@
 package ru.practicum.ewm.statistic.dto;
 
-import java.time.format.DateTimeFormatter;
-
 public class Formats {
 
     public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
