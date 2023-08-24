56b2b61783172fbdc26dc8bed5ac805370adec5c
Nikita Komochkov <ascii2@yandex.ru>
2023-08-24 21:51:38 +0300

    refactor: pull code update after review v1.1

--- main/src/main/java/ru/practicum/ewm/main/user/service/UserServiceImpl.java
+++ main/src/main/java/ru/practicum/ewm/main/user/service/UserServiceImpl.java
@@ -3,12 +3,14 @@ package ru.practicum.ewm.main.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.ewm.main.event.model.Event;
+import ru.practicum.ewm.main.event.repository.EventRepository;
+import ru.practicum.ewm.main.event.repository.RateDAO;
 import ru.practicum.ewm.main.user.dto.NewUserRequestDto;
 import ru.practicum.ewm.main.user.dto.UserDto;
 import ru.practicum.ewm.main.user.mapper.UserMapper;
 import ru.practicum.ewm.main.user.model.User;
 import ru.practicum.ewm.main.user.repository.UserRepository;
-import ru.practicum.ewm.main.event.repository.RateDAO;
 
 import java.util.List;
 import java.util.Map;
